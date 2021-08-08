b59ebb74282e86631ad0fd2a35bb5f37cc136731
he55 <40764272+he55@users.noreply.github.com>
2021-08-08 14:52:20 +0800

    Update PInvoke.cs

--- DyDesktopWinForms/PInvoke.cs
+++ DyDesktopWinForms/PInvoke.cs
@@ -5,11 +5,9 @@ namespace DyDesktopWinForms
 {
     public static class PInvoke
     {
-
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public delegate bool WNDENUMPROC(IntPtr hwnd, IntPtr lParam);
-
 
         [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
         public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
@@ -22,18 +20,20 @@ namespace DyDesktopWinForms
             string windowTitle);
 
         [DllImport("User32.dll", SetLastError = true)]
-        public static extern IntPtr SendMessageTimeout(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam, int flags, int timeout, out IntPtr pdwResult);
+        public static extern IntPtr SendMessageTimeout(
+            IntPtr hWnd,
+            int msg,
+            IntPtr wParam,
+            IntPtr lParam,
+            int flags,
+            int timeout,
+            out IntPtr pdwResult);
 
         [DllImport("User32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool EnumWindows(WNDENUMPROC lpEnumFunc, IntPtr lParam);
 
-
         [DllImport("User32.dll", SetLastError = true)]
         public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-
-
-
     }
-
 }
