d4cbe007bdf4d323f295b3f6a924d232ea51c3e2
he55 <40764272+he55@users.noreply.github.com>
2021-08-09 14:15:47 +0800

    Update Form1.cs

--- DyDesktopWinForms/Form1.cs
+++ DyDesktopWinForms/Form1.cs
@@ -13,12 +13,30 @@ namespace DyDesktopWinForms
     public partial class Form1 : Form
     {
         private VideoWindow videoWindow;
-        private bool bf;
 
         public Form1()
         {
             InitializeComponent();
             this.MaximumSize = this.MinimumSize = this.Size;
+        }
+
+        private void CreateVideoWindow()
+        {
+            if (videoWindow == null)
+            {
+                videoWindow = new VideoWindow();
+                videoWindow.FullScreen();
+                videoWindow.Show();
+
+                IntPtr workerWindowHandle = DesktopWorker.GetWorkerWindowHandle();
+                PInvoke.SetParent(videoWindow.Handle, workerWindowHandle);
+
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                checkBox1.Enabled = true;
+                trackBar1.Enabled = true;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -27,7 +45,7 @@ namespace DyDesktopWinForms
             openFileDialog.Filter = "MP4 Files (*.mp4)|*.mp4";
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                meth();
+                CreateVideoWindow();
                 videoWindow.Source = new Uri(openFileDialog.FileName, UriKind.Absolute);
                 videoWindow.Play();
             }
@@ -54,30 +72,16 @@ namespace DyDesktopWinForms
             videoWindow.Volume = trackBar1.Value / 10.0;
         }
 
-        private void meth()
-        {
-            if (!bf)
-            {
-                bf = true;
-
-                videoWindow = new VideoWindow();
-                videoWindow.FullScreen();
-                videoWindow.Show();
-
-                IntPtr workerWindowHandle = DesktopWorker.GetWorkerWindowHandle();
-                PInvoke.SetParent(videoWindow.Handle, workerWindowHandle);
-
-                button3.Enabled = true;
-                button4.Enabled = true;
-                checkBox1.Enabled = true;
-                trackBar1.Enabled = true;
-            }
-        }
-
         private void button5_Click(object sender, EventArgs e)
         {
             videoWindow.Close();
             videoWindow = null;
+
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            checkBox1.Enabled = false;
+            trackBar1.Enabled = false;
         }
     }
 }
