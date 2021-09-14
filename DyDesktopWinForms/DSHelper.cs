7cb0f60fee3fd597f044893227139c7a35dec9b0
he55 <40764272+he55@users.noreply.github.com>
2021-09-14 14:28:00 +0800

    Update DSHelper.cs

--- DyDesktopWinForms/DSHelper.cs
+++ DyDesktopWinForms/DSHelper.cs
@@ -22,22 +22,22 @@ namespace DyDesktopWinForms
         public static bool CheckStartOnBoot()
         {
             RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(registryStartupLocation);
-           bool startOnBoot = startupKey.GetValue("DyDesktopWinForms") != null;
+            bool startOnBoot = startupKey.GetValue("DyDesktopWinForms") != null;
             startupKey.Close();
             return startOnBoot;
-        }
-
-        public static void DelToggleStartOnBoot()
-        {
-            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(registryStartupLocation, true);
-            startupKey.DeleteValue("DyDesktopWinForms");
-            startupKey.Close();
         }
 
         public static void StToggleStartOnBoot()
         {
             RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(registryStartupLocation, true);
             startupKey.SetValue("DyDesktopWinForms", Application.ExecutablePath + " -c");
+            startupKey.Close();
+        }
+
+        public static void DelToggleStartOnBoot()
+        {
+            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(registryStartupLocation, true);
+            startupKey.DeleteValue("DyDesktopWinForms");
             startupKey.Close();
         }
     }
