677e9a6584b981941a0f57c7ebe0899c5c92324a
he55 <40764272+he55@users.noreply.github.com>
2021-09-14 14:34:35 +0800

    Update DSHelper.cs

--- DyDesktopWinForms/DSHelper.cs
+++ DyDesktopWinForms/DSHelper.cs
@@ -10,23 +10,6 @@ namespace DyDesktopWinForms
         const string registryStartupLocation = @"Software\Microsoft\Windows\CurrentVersion\Run";
         static string str;
 
-        public static string met()
-        {
-            if (str == null)
-            {
-                str = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".DyDesktopWinForms");
-            }
-            return str;
-        }
-
-        public static bool CheckStartOnBoot()
-        {
-            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(registryStartupLocation);
-            bool startOnBoot = startupKey.GetValue("DyDesktopWinForms") != null;
-            startupKey.Close();
-            return startOnBoot;
-        }
-
         public static void StToggleStartOnBoot()
         {
             RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(registryStartupLocation, true);
@@ -39,6 +22,23 @@ namespace DyDesktopWinForms
             RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(registryStartupLocation, true);
             startupKey.DeleteValue("DyDesktopWinForms");
             startupKey.Close();
+        }
+
+        public static bool CheckStartOnBoot()
+        {
+            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(registryStartupLocation);
+            bool startOnBoot = startupKey.GetValue("DyDesktopWinForms") != null;
+            startupKey.Close();
+            return startOnBoot;
+        }
+
+        public static string met()
+        {
+            if (str == null)
+            {
+                str = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".DyDesktopWinForms");
+            }
+            return str;
         }
     }
 }
