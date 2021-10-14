7b963b66f9bc5bd595ec18589f8164ee674e1665
he55 <40764272+he55@users.noreply.github.com>
2021-10-14 10:24:33 +0800

    add webview2

--- DyDesktopWinForms/WebWindow.xaml.cs
+++ DyDesktopWinForms/WebWindow.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DyDesktopWinForms
 {
@@ -22,6 +11,12 @@ namespace DyDesktopWinForms
         public WebWindow()
         {
             InitializeComponent();
+        }
+
+        public Uri Source
+        {
+            get => webView2.Source;
+            set => webView2.Source = value;
         }
     }
 }
