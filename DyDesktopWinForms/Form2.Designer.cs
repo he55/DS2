8fd699090b6040ba773111fd490df2b1c2231f70
he55 <40764272+he55@users.noreply.github.com>
2021-10-14 11:05:32 +0800

    Update Form2.Designer.cs

--- DyDesktopWinForms/Form2.Designer.cs
+++ DyDesktopWinForms/Form2.Designer.cs
@@ -37,7 +37,7 @@ namespace DyDesktopWinForms
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(355, 86);
+            this.label1.Location = new System.Drawing.Point(22, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 0;
@@ -45,14 +45,14 @@ namespace DyDesktopWinForms
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(357, 147);
+            this.textBox1.Location = new System.Drawing.Point(24, 52);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.Size = new System.Drawing.Size(208, 21);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(357, 198);
+            this.button1.Location = new System.Drawing.Point(98, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -63,11 +63,14 @@ namespace DyDesktopWinForms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(285, 136);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
             this.ResumeLayout(false);
             this.PerformLayout();
