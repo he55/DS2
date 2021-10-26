4f4117129a1adc7bf67d358a8e3809a8c3c3ad4d
he55 <40764272+he55@users.noreply.github.com>
2021-10-26 09:13:21 +0800

    fix

--- DyDesktopWinForms/PInvoke.cs
+++ DyDesktopWinForms/PInvoke.cs
@@ -30,4 +30,16 @@ namespace DyDesktopWinForms
         [DllImport("ProjectBr.dll")]
         public static extern void reLastPos();
     }
+
+    public struct RECT
+    {
+
+        public int left;
+
+        public int top;
+
+        public int right;
+
+        public int bottom;
+    }
 }
