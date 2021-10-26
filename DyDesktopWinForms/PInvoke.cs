fbb89b00df91e937d19d3d455587c773eaebaf31
he55 <40764272+he55@users.noreply.github.com>
2021-10-26 09:13:51 +0800

    Update PInvoke.cs

--- DyDesktopWinForms/PInvoke.cs
+++ DyDesktopWinForms/PInvoke.cs
@@ -33,13 +33,9 @@ namespace DyDesktopWinForms
 
     public struct RECT
     {
-
         public int left;
-
         public int top;
-
         public int right;
-
         public int bottom;
     }
 }
