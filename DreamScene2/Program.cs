b562ef6bbdeaacaf423c8e17e4abc31bdd35f801
he55 <40764272+he55@users.noreply.github.com>
2021-12-02 12:21:39 +0800

    Update Program.cs

--- DreamScene2/Program.cs
+++ DreamScene2/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -19,7 +20,23 @@ namespace DreamScene2
                 return;
             }
 
-            Directory.SetCurrentDirectory(Application.StartupPath);
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string[] vs = assembly.GetManifestResourceNames();
+            foreach (string vsName in vs)
+            {
+                if (vsName.EndsWith(".dll"))
+                {
+                    string filename = vsName.Substring(Constant.projectName.Length + 1);
+                    string pa = Path.Combine(Application.StartupPath, filename);
+                    using (FileStream fileStream = File.Create(pa))
+                    {
+                        Stream stream = assembly.GetManifestResourceStream(vsName);
+                        stream.CopyTo(fileStream);
+                    }
+                }
+            }
+
+            //Directory.SetCurrentDirectory(Application.StartupPath);
 
             string extPath = Helper.ExtPath();
             if (!Directory.Exists(extPath))
